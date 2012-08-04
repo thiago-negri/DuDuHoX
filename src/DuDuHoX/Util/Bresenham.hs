e57adfbf9ccda4a647ca9834cb252edb515f0251
Thiago Negri <evohunz@gmail.com>
2012-08-04 19:15:25 -0300

    Changed Signal to Bool
    Signal type changed to a type synonym of Bool

--- src/DuDuHoX/Util/Bresenham.hs
+++ src/DuDuHoX/Util/Bresenham.hs
@@ -14,19 +14,15 @@ type P = Int
 type Q = Int
 type EPS = Int
 
-data Signal = Zero | One
+type Signal = Bool
 type QuadraticWave = [Signal]
 
 type BalancedWordStrategy = (QuadraticWave, Step)
 
-asInt :: Signal -> Int
-asInt Zero = 0
-asInt One = 1
-
 balancedWord :: P -> Q -> EPS -> QuadraticWave
 balancedWord p q eps = signal : balancedWord p q newContrivedEps
     where
-        (signal, newContrivedEps) = if newEps < q then (Zero, newEps) else (One, newEps - q)
+        (signal, newContrivedEps) = if newEps < q then (False, newEps) else (True, newEps - q)
         newEps = eps + p
 
 getStrategy :: Point -> Point -> BalancedWordStrategy
@@ -36,8 +32,9 @@ getStrategy (x0, y0) (x1, y1) = (balance, step)
                      | otherwise       = (abs dx, abs dy, verticalStep)
         balance = balancedWord p q 0
         (dx, dy) = (x1 - x0, y1 - y0)
-        horizontalStep signal (x, y) = (x + signum dx,                y + signum dy * asInt signal)
-        verticalStep   signal (x, y) = (x + signum dx * asInt signal, y + signum dy)
+        horizontalStep signal (x, y) = (x + signum dx,                y + block signal (signum dy))
+        verticalStep   signal (x, y) = (x + block signal (signum dx), y + signum dy)
+        block signal a = if signal then a else 0 
 
 digitalLine :: Point -> Point -> Line
 digitalLine a b = takeWhile (/= b) $ walk balance a
